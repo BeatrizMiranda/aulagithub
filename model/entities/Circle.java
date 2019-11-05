9af39915ba35e241f293c0285479b155f53eb1d1
beatrizmiranda <biia.mfs@gmail.com>
2019-11-04 22:18:16 -0200

    commit

---
+++ model/entities/Circle.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.entities;
+
+import model.enums.Color;
+
+public class Circle extends AbstractShape {
+
+	private Double radius;
+
+	public Circle(Color color, Double radius) {
+		super(color);
+		this.radius = radius;
+	}
+
+	public Double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(Double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+}
